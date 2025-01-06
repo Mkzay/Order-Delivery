badb07689da85e9ee02883aa0a0ae1ee9c900361
Mkzay <80535194+Mkzay@users.noreply.github.com>
2025-01-06 19:12:54 +0100

    key update

--- components/home/restuarants.jsx
+++ components/home/restuarants.jsx
@@ -1,26 +1,32 @@
 const Restaurant = () => {
   const categories = [
     {
+      key: 1,
       name: "Pizzas",
       image: "images/pizza.png",
     },
     {
+      key: 2,
       name: "Burgers & Fast Food",
       image: "images/burger.png",
     },
     {
+      key: 3,
       name: "Salads",
       image: "images/salad.png",
     },
     {
+      key: 4,
       name: "Soups",
       image: "images/soup.png",
     },
     {
+      key: 5,
       name: "Breakfast",
       image: "images/breakfast.png",
     },
     {
+      key: 6,
       name: "Pasta & Casuals",
       image: "images/pasta.png",
     },
@@ -33,7 +39,7 @@ const Restaurant = () => {
       </h1>
       <div className="lg:hidden flex items-center justify-center flex-wrap rounded-xl gap-2 w-full lg:flex-nowrap lg:justify-start lg:items-center lg:overflow-x-auto lg:whitespace-nowrap lg:scrollbar-hide">
         {categories.map((category) => (
-          <div className="w-[189px] h-[211px] bg-accent lg:flex-shrink-0 rounded-xl lg:w-96 lg:h-72">
+          <div key={category.key} className="w-[189px] h-[211px] bg-accent lg:flex-shrink-0 rounded-xl lg:w-96 lg:h-72">
             <img src={category.image} alt={category.name} />
             <p className="font-bold text-orange text-xs p-2">{category.name}</p>
           </div>
